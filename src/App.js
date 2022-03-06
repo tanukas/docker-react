a24afa17f34710ad9b2cfe6e83371172d18e59df
tanukas <tanuka.shrivastava@gmail.com>
2022-03-06 19:03:47 +0530

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         What just happened? Hello? Anybody Home?
+         Welcome Home!
         </p>
         <a
           className="App-link"
